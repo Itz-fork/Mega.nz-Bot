06684c05bf5fa5b05988c5337619b29b0cb19aa7
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-13 20:43:32 +0530

    1.9GB file size limt :|

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 123456))
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    TG_MAX_SIZE = 2097152000
+    TG_MAX_SIZE = 2040108421
