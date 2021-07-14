df115774f89b795bdc9c0218af2deed8cd5c94df
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-14 22:33:35 +0530

    fuk

--- megadl/mega_dl.py
+++ megadl/mega_dl.py
@@ -126,7 +126,7 @@ async def megadl(_, message: Message):
 
 
 # Replying If There is no mega url in the message
-@Client.on_message(~filters.command(["info"]) & ~filters.regex(MEGA_REGEX) & filters.private)
+@Client.on_message(~filters.command(["info", "upload"]) & ~filters.regex(MEGA_REGEX) & filters.private)
 async def nomegaurl(_, message: Message):
   # Auth users only
     if message.from_user.id not in Config.AUTH_USERS:
