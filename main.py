6ecbc6ddffa474ec4a5da226d7e4cc776d6d96a0
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-14 08:35:33 +0530

    .

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Copyright (c) 2021 Itz-fork
 # Don't kang this else your dad is gae
 
-from pyrogram import Client
+from pyrogram import Client, idle
 from config import Config
 
 
-MeganzBot = Client(
-    "Meganz Bot",
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="megadl")
-)
-MeganzBot.run()
+if __name__ == "__main__" :
+    Megadlbot = Client(
+        "Nexa MegaDl",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=dict(root="megadl")
+    )
+    Megadlbot.start()
+    idle()
