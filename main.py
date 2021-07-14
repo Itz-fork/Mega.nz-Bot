bcab99f039efffedfc25db6c50c5c444e1c9cc82
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-14 14:09:31 +0530

    dir fix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2021 Itz-fork
 # Don't kang this else your dad is gae
+import os
 
 from pyrogram import Client, idle
 from config import Config
 
 if __name__ == "__main__" :
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="megadl")
     app = Client(
         "MegaBot",
