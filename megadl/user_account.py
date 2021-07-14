6192a6dfcdf4add8b8e5d035b1539812621a55d0
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-14 22:31:23 +0530

    jojo

--- megadl/user_account.py
+++ megadl/user_account.py
@@ -3,6 +3,8 @@
 # This plugin is working only if you Login with Mega Account
 
 import json
+import os
+import time
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -11,10 +13,11 @@ from hurry.filesize import size
 from megadl.account import m
 from megadl.mega_dl import GITHUB_REPO
 from config import Config
+from megadl.mega_help import progress_for_pyrogram, humanbytes
 
 # Get Mega user Account info
 @Client.on_message(filters.command("info") & filters.private)
-async def nomegaurl(_, message: Message):
+async def accinfo(_, message: Message):
   if message.from_user.id not in Config.AUTH_USERS:
     await message.reply_text("**Sorry this bot isn't a Public Bot 🥺! But You can make your own bot ☺️, Click on Below Button!**", reply_markup=GITHUB_REPO)
     return
@@ -37,3 +40,38 @@ async def nomegaurl(_, message: Message):
   used_space = size(bused_space)
   free_space = size(bfree_space)
   await message.reply_text(f"**~ Your User Account Info ~** \n\n▪ **Account Name:** `{acc_name}` \n▪ **Email:** `{acc_email}` \n▪ **Storage,** \n - **Total:** `{total_space}` \n - **Used:** `{used_space}` \n - **Free:** `{free_space}` \n▪ **Quota:** `{acc_quota} MB`")
+
+
+# uplaod files
+@Client.on_message(filters.command("upload") & filters.private)
+async def uptomega(client: Client, message: Message):
+  if message.from_user.id not in Config.AUTH_USERS:
+    await message.reply_text("**Sorry this bot isn't a Public Bot 🥺! But You can make your own bot ☺️, Click on Below Button!**", reply_markup=GITHUB_REPO)
+    return
+  if Config.USER_ACCOUNT == "False":
+    await message.reply_text("You didn't setup a Mega.nz Account to Get details!")
+    return
+  todownfile = message.reply_to_message
+  if not todownfile:
+    await message.reply_text("**Please reply to a Media File to Upload!**")
+    return
+  if todownfile.media is None:
+    await message.reply_text("Sorry, I can't Upload Text to Mega! Please reply to a Media File!")
+    return
+  try:
+    start_time = time.time()
+    megaupmsg = await message.reply_text("**Starting to Download The Content to My Server! This may take while 😴**")
+    toupload = await client.download_media(message=todownfile, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", megaupmsg, start_time))
+    await megaupmsg.edit("**Successfully Downloaded the File!**")
+    await megaupmsg.edit("**Trying to Upload to Mega.nz**")
+    uploadfile = m.upload(toupload)
+    link = m.get_upload_link(uploadfile)
+    await megaupmsg.edit(f"**Successfully Uploaded To Mega.nz** \n\n**Link:** `{link}` \n\n**Powered by @NexaBotsUpdates**")
+    if toupload is not None:
+      os.remove(toupload)
+  except Exception as e:
+    await megaupmsg.edit(f"**Error:** `{e}`")
+    if toupload is not None:
+      os.remove(toupload)
+    else:
+      print("Why this file is gae?")
