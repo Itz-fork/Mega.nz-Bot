46db1cf9abbed26383d26ee778cd2865e6354dc2
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-14 08:38:24 +0530

    Mega Account

---
+++ megadl/account.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2021 Itz-fork
+# Don't kang this else your dad is gae
+
+from mega import Mega
+from config import Config
+
+email = Config.MEGA_EMAIL
+password = Config.MEGA_PASSWORD
+
+# Mega User Account
+if USE_ACCOUNT is "True":
+  m = mega.login(email, password)
+else:
+  m = mega.login()
