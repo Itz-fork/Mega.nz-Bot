db9f455808ce214ecba328ce84b276491aed6549
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-18 00:04:11 +0530

    Update mega_dl.py

--- megadl/mega_dl.py
+++ megadl/mega_dl.py
@@ -136,23 +136,3 @@ async def nomegaurl(_, message: Message):
         return
     else:
       await message.reply_text("Sorry, I can't find a valid mega.nz url in your message! Can you check it again?")
-
-# Start message
-@Client.on_message(filters.command("start"))
-async def startcmd(megabot: Client, message: Message):
-  # Auth users only
-    if message.from_user.id not in Config.AUTH_USERS:
-        await message.reply_text("**Sorry this bot isn't a Public Bot 🥺! But You can make your own bot ☺️, Click on Below Button!**", reply_markup=GITHUB_REPO)
-        return
-    else:
-      await message.reply_text(f"Hello, Nice to Meet You **{message.from_user.first_name}** 😇!, \n\nI'm **@{(await megabot.get_me()).username}**, Your Own Mega.nz Uploader 😉! \n\nIf You don't Know how to work with me hit on /help command 😁")
-
-# Help command
-@Client.on_message(filters.command("help"))
-async def helpcmd(megabot: Client, message: Message):
-  # Auth users only
-    if message.from_user.id not in Config.AUTH_USERS:
-        await message.reply_text("**Sorry this bot isn't a Public Bot 🥺! But You can make your own bot ☺️, Click on Below Button!**", reply_markup=GITHUB_REPO)
-        return
-    else:
-      await message.reply_text(f"Hi **{message.from_user.first_name}** 😇!, \n\n\n**📥 Download Mega.nz Links** \n - Just send me a valid Mega.nz Link. (Folder Not Supported) \n\n**📤 Upload to Mega.nz** \n - First Send or Forward a File to Me. \n - Then Reply to that file with `/upload` command \n\n**Powered by @NexaBotsUpdates**")
