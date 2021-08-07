3fa6864eda982b38ec2f9f4fe4b93e78f1773d54
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-07 09:07:18 +0530

    all new config.py

--- config.py
+++ config.py
@@ -3,14 +3,60 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 123456))
+    APP_ID = int(os.environ.get("APP_ID", 1234567))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1340254734").split())
     DOWNLOAD_LOCATION = "./NexaBots"
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "False")
+    LOGS_CHANNEL = os.environ.get("LOGS_CHANNEL")
     TG_MAX_SIZE = 2040108421
-    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "")
     # Mega User Account
-    USER_ACCOUNT = os.environ.get("USER_ACCOUNT", "False")
     MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "")
     MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "")
+
+# Text Prints
+B_START_TEXT = """
+   __  ___                                     ___       __ 
+  /  |/  /__ ___ ____ _      ___  ___   ____  / _ )___  / /_
+ / /|_/ / -_) _ `/ _ `/ _   / _ \/_ /  /___/ / _  / _ \/ __/
+/_/  /_/\__/\_, /\_,_/ (_) /_//_//__/       /____/\___/\__/ 
+           /___/
+
+Process: {}
+"""
+
+PROCESS_TEXT = """
+Process: {}
+"""
+
+LOGGED_AS_USER = """
+Successfully Logged Into Mega.nz User Account
+"""
+
+LOGIN_ERROR_TEXT = """
+Can't Get Mega Email and Password Login as an Anonymouse User
+"""
+
+ERROR_TEXT = """
+ _____                     
+| ____|_ __ _ __ ___  _ __ 
+|  _| | '__| '__/ _ \| '__|
+| |___| |  | | | (_) | |   
+|_____|_|  |_|  \___/|_|  
+
+Log: {}
+
+Save the Log file and Send it to @Nexa_bots for Help :)
+"""
+
+START_TEXT="""
+    _   __                   ____        __      
+   / | / /__  _  ______ _   / __ )____  / /______
+  /  |/ / _ \| |/_/ __ `/  / __  / __ \/ __/ ___/
+ / /|  /  __/>  </ /_/ /  / /_/ / /_/ / /_(__  ) 
+/_/ |_/\___/_/|_|\__,_/  /_____/\____/\__/____/ 
+
+
+Bot is Running Now! Join @NexaBotsUpdates
+"""
