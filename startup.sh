18c496e0f6febb2f53b519fc49b4c30968aec905
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-12-28 23:17:35 +0530

    Sed

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+echo "
+█▀▄▀█ █▀▀ █▀▀ ▄▀█   █▄ █ ▀█ 
+█ ▀ █ ██▄ █▄█ █▀█ ▄ █ ▀█ █▄
+
+        █▄▄ █▀█ ▀█▀
+        █▄█ █▄█  █
+
+
+Copyright (c) 2021 Itz-fork | @NexaBotsUpdates
+"
+
+echo ">> Starting the main repo"
+python3 -m megadl
