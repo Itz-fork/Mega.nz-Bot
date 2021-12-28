eb84e12352b8f953aef8c8673374ea84dd4f2767
Itz-fork <git.itzfork@gmail.com>
2021-12-28 23:49:23 +0530

    Sed * 5 * (2*99)

--- megadl/helpers_nexa/up_helper.py
+++ megadl/helpers_nexa/up_helper.py
@@ -23,16 +23,20 @@ async def guess_and_send(input_file, chat_id, thumb_path, message):
     in_file = f"{input_file}"
     start_time = time()
     guessedfilemime = filetype.guess(in_file)
+    try:
+        mention = (await Client.get_me()).mention
+    except:
+        mention = "**Your Mega.nz-Bot!**"
     if not guessedfilemime or not guessedfilemime.mime:
-        return await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+        return await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
     try:
         filemimespotted = guessedfilemime.mime
         # For gifs
         if re.search(r'\bimage/gif\b', filemimespotted):
-            await Client.send_animation(chat_id=chat_id, animation=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+            await Client.send_animation(chat_id=chat_id, animation=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
         # For images
         elif re.search(r'\bimage\b', filemimespotted):
-            await Client.send_photo(chat_id=chat_id, photo=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+            await Client.send_photo(chat_id=chat_id, photo=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
         # For videos
         elif re.search(r'\bvideo\b', filemimespotted):
             viddura = await get_vid_duration(input_video=in_file)
@@ -40,12 +44,12 @@ async def guess_and_send(input_file, chat_id, thumb_path, message):
             if os.path.exists(thumbnail_path):
                 os.remove(thumbnail_path)
             await run_shell_cmds(f"ffmpeg -i {in_file} -ss 00:00:01.000 -vframes 1 {thumbnail_path}")
-            await Client.send_video(chat_id=chat_id, video=in_file, duration=viddura, thumb=thumbnail_path, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+            await Client.send_video(chat_id=chat_id, video=in_file, duration=viddura, thumb=thumbnail_path, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
         # For audio
         elif re.search(r'\baudio\b', filemimespotted):
-            await Client.send_audio(chat_id=chat_id, audio=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+            await Client.send_audio(chat_id=chat_id, audio=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
         else:
-            await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+            await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
     except Exception as e:
         print(e)
-        await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await Client.get_me()).mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
+        await Client.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {mention}", progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", message, start_time))
