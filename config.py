9c3069c9168dc4356dea11c7834c4b29228ab641
Itz-fork <git.itzfork@gmail.com>
2022-05-27 11:38:23 +0530

    Get "IS_PUBLIC_BOT" env as a boolean

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    IS_PUBLIC_BOT = bool(os.environ.get("IS_PUBLIC_BOT", False))
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT").lower() in ["True", "true"]
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL")) if os.environ.get("LOGS_CHANNEL") else None
     # DON'T CHANGE THESE 2 VARS
     DOWNLOAD_LOCATION = "./NexaBots"
