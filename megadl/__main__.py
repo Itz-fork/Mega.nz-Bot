fd4a38c76fc085d984e29c5c60bbc149e5f8e37b
Itz-fork <git.itzfork@gmail.com>
2023-12-21 14:35:55 +0530

    updated plugin path

--- megadl/__main__.py
+++ megadl/__main__.py
@@ -12,7 +12,7 @@ meganzbot = Client(
     bot_token=Config.BOT_TOKEN,
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
-    plugins=dict(root="bot/modules"),
+    plugins=dict(root="megadl/modules"),
     sleep_threshold=10,
 )
 
