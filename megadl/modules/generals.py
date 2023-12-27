d449b4484adf6b8006943c875ca3cbd840342f9c
Itz-fork <git.itzfork@gmail.com>
2023-12-27 22:21:38 +0530

    added help comand

--- megadl/modules/generals.py
+++ megadl/modules/generals.py
@@ -15,6 +15,29 @@ from megadl import MeganzClient
 async def start_msg(_: MeganzClient, msg: Message):
     await msg.reply_text(
         f"""
-    Hi `{msg.from_user.first_name}`, this is your personal Mega.nz bot!
+Hi `{msg.from_user.first_name}` 👋, I'm [Mega.nz-Bot](https://github.com/Itz-fork/Mega.nz-Bot)!
+
+I can help you download, upload files or folders from telegram. Not sure what to do? Check /help for more info 😇
     """
+    )
+
+
+@MeganzClient.on_message(filters.command("help"))
+@MeganzClient.handle_checks
+async def help_msg(_: MeganzClient, msg: Message):
+    await msg.reply_text(
+        f"""
+**How do I login?**
+  Send /login command and enter your details when I ask you. Don't worry we encrypt your data before sending it anywhere 🤗
+
+** How to download from mega link?**
+  It's very easy. Just send the link you want to download and I'll download it for you 😉
+
+**How to upload files to Mega.nz?**
+  Just send me the files and I'll ask you whether you want to upload it or not. Same goes for direct download links 😎
+
+
+  
+**Made with ❤️ by @NexaBotsUpdates**
+      """
     )
