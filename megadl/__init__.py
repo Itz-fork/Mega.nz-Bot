f463324140c11988420cd6e239095cfa3883d4e4
Itz-fork <git.itzfork@gmail.com>
2024-05-27 00:21:29 +0530

    patch: [1.0.81]
    load_dotenv only if .env file is present

--- megadl/__init__.py
+++ megadl/__init__.py
@@ -3,6 +3,9 @@
 # Project: https://github.com/Itz-fork/Mega.nz-Bot
 # Description: __init__.py
 
+import os
+import logging
+
 # start msg
 print("Mega.nz Bot - Cypher is starting...")
 
@@ -11,8 +14,12 @@ print("Mega.nz Bot - Cypher is starting...")
 from dotenv import load_dotenv
 print("--------------------")
 print("> Loading config")
-load_dotenv()
+if os.path.isfile('.env'):
+    load_dotenv()
+else:
+    logging.warning("WARNING: No .env file found")
 
 # client
 from .helpers.cypher import MeganzClient
+import os
 CypherClient: "MeganzClient" = MeganzClient()
