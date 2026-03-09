6f1e91d8eec3c4d365840bca660406d0d311cf92
Hirusha Himath <git.itzfork@gmail.com>
2026-03-09 13:52:53 +0530

    patch: fixes to database
    - Fixes major flaw in "mega_logout" which deletes the user document on log out. This couldn't have been used to bypass bans due to the logical flaw of the system but its a nice to have.
    - Fixes in "update_proxy" which uses upsert=True. Again nothing major and worst case cannot happen but hey, nice to have.

--- megadl/helpers/database.py
+++ megadl/helpers/database.py
@@ -14,7 +14,6 @@ class CypherDB:
         self.coll_users = self.mongoc["mega_nz"]["users"]
 
     # <<<<<<<<<< Active user functions >>>>>>>>>> #
-
     async def add(self, user_id: int):
         result = await self.mongoc.find_one_and_update_async(
             self.coll_users,
@@ -74,7 +73,7 @@ class CypherDB:
         if not docu:
             return None
         if use_acc:
-            return docu if not "" in {docu["email"], docu["password"]} else None
+            return docu if not "" in (docu["email"], docu["password"]) else None
         else:
             return docu
 
@@ -86,26 +85,30 @@ class CypherDB:
             {"status": {"banned": True, "reason": reason}},
         )
 
-    async def unban_user(self, user_id: int):
+    async def unban_user(self, user_id: int, reason: str = "Got unbanned"):
         await self.mongoc.update_async(
             self.coll_users,
             {"_id": user_id},
-            {"status": {"banned": False, "reason": "Got unbanned"}},
+            {"status": {"banned": False, "reason": reason}},
         )
 
     # <<<<<<<<<< Mega functions >>>>>>>>>> #
-
     async def mega_login(self, user_id: int, email: str, password: str):
         await self.mongoc.update_async(
             self.coll_users,
             {"_id": user_id},
             {"email": email, "password": password},
-            upsert=True,
         )
 
     async def mega_logout(self, user_id: int):
-        await self.mongoc.delete_async(self.coll_users, {"_id": user_id})
+        await self.mongoc.update_async(
+            self.coll_users,
+            {"_id": user_id},
+            {"email": "", "password": ""},
+        )
 
+    # this isnt used anywhere as far as from what i can tell but im not gonna risk it by deleting anything either
+    # fuck it
     async def how_many(self):
         return (user["user_id"] async for user in self.coll_users.find({}))
 
@@ -115,7 +118,6 @@ class CypherDB:
             self.coll_users,
             {"_id": user_id},
             {"proxy": proxy},
-            upsert=True,
         )
 
     async def get_proxy(self, user_id: int):
